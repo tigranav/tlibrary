a887676378fd91335f4903e1d63fbae9cf6fd0be
tigranav <427>
2018-10-10 06:08:28 +0300

    day2

--- books/models.py
+++ books/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    old_id = models.BigIntegerField(help_text="old id")
+    title = models.CharField(max_length=255, help_text="Title of books")
+    author = models.CharField(max_length=255, help_text="authors")
+    year = models.IntegerField(help_text="year")
+    pages = models.IntegerField(help_text="number of pages")
+    description = models.CharField(max_length=4000, help_text="Description")
+    ISBN = models.CharField(max_length=40, help_text="ISBN")
+    inserted = models.DateTimeField(auto_now=True)
+
+    # Metadata
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        """
+        String for representing the MyModelName object (in Admin site etc.)
+        """
+        return self.title+" "+self.author
+
--- books/views.py
+++ books/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.http import Http404
+from .models import Book
+from django.template import loader
+from django.shortcuts import  render
 # Create your views here.
+
+
+
+def index(request):
+    all_books = Book.objects.all()
+    template = loader.get_template('books/index.html')
+    context = {
+        'all_books': all_books,
+    }
+    return render(request,'books/index.html',context)
+
+def detail(request,book_id):
+    try:
+        book = Book.objects.get(pk=book_id)
+    except Book.DoesNotExist:
+        raise Http404("Book Does not exist")
+    context = {'book': book}
+    return render(request, 'books/detail.html', context)
